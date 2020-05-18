da738779f7abb0e56f876dc315cf72ca062dc339
hafsat-max <hafsahbusari69@gmail.com>
2020-05-18 22:46:04 +0100

    created a new project

--- js/jas.js
+++ js/jas.js
@@ -1,33 +1,23 @@
 
 
-
-        var subcategory = {
-            ABIA: ["1", "2", "3"],
-            ADAMAWA: ["4", "5", "6"],
-            AKWAIBOM: ["Y", "DSS", "SS"],
-
-            ANAMBRA: ["EKJ", "EKJ", "EKJ"],
-
-            BAUCHI:["EKJ", "EKJ", "EKJ"],
-
-            BAYELSA:["EKJ", "EKJ", "EKJ"],
-
-            BENUE  : ["EKJ", "EKJ", "EKJ"],
-
-            BORNO   : ["EKJ", "EKJ", "EKJ"],
-
-            CROSSRIVER    : ["EKJ", "EKJ", "EKJ"],
-
-            DELTA: ["EKJ", "EKJ", "EKJ"],
-
-            EKITI: ["EKJ", "EKJ", "EKJ"],
-
-            EDO: ["EKJ", "EKJ", "EKJ"]
-                
+//
+//        var subcategory = {
+//            NORTHCENTRAL: ["BENUE STATE", "FCT", "KOGI STATE", "NIGER STATE", "PLATEAU"],
+//            NORTHWEST: ["KADUNA STATE", "KANO STATE", "KATSINA STATE", "SOKOTO SATE", "ZAMFARA STATE"],
+//            NORTHEAST: ["ADAMAWA SATE", "BAUCHI STATE", "BORNO STATE", "GOMBE STATE", "TARABA STATE", "YOBE STATE"],
+//            SOUTHSOUTH: ["AKWAIBOM STATE", "BAYELSA STATE", "CROSSRIVER STATE", "DELTA STATE", "EDO STATE", "RIVER STATE"]
+//        
+//        }
+        
+        var subCategory = [
+            {name: 'NORTHCENTRAL', subValues:[{state: "BENUE STATE", phone: 09018602439}, {state: "FCT", phone: 08099936312}, {state: "KOGI STATE", phone: 0704485619}, {state: "KWARA STATE", phone: 090620100001},{state: "Niger", phone:08038246018}, {state: "PLATEAU", phone: 07032864444}]},
             
+            {name: 'NORTHWEST', subValues:[{state: "KADUNA STATE", phone: 08977}, {state: "FCT", phone: 089787}, {state: "KOGI STATE", phone: 08977}, {state: "NIGER STATE", phone: 08977}, {state: "PLATEAU", phone: 94889}]},
             
-
-        }
+            {name: 'NORTHEAST', subValues:[{state: "ADAMAWA STATE", phone: 08977}, {state: "FCT", phone: 089787}, {state: "KOGI STATE", phone: 08977}, {state: "NIGER STATE", phone: 08977}, {state: "PLATEAU", phone: 94889}]},
+            
+            {name: 'SOUTHSOUTH', subValues:[{state: "AKWA-IBOM STATE", phone: 08977}, {state: "FCT", phone: 089787}, {state: "KOGI STATE", phone: 08977}, {state: "NIGER STATE", phone: 08977}, {state: "PLATEAU", phone: 94889}]}
+        ]
 
         function makeSubmenu(value) {
             if (value.length == 0) document.getElementById("categorySelect").innerHTML = "<option></option>";
