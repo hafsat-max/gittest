5a090e2a8a29c9bb388089aa8b2413c8f3c1bf2b
hafsat-max <hafsahbusari69@gmail.com>
2020-05-18 23:26:42 +0100

    modified jas.js

--- js/jas.js
+++ js/jas.js
@@ -10,13 +10,20 @@
 //        }
         
         var subCategory = [
-            {name: 'NORTHCENTRAL', subValues:[{state: "BENUE STATE", phone: 09018602439}, {state: "FCT", phone: 08099936312}, {state: "KOGI STATE", phone: 0704485619}, {state: "KWARA STATE", phone: 090620100001},{state: "Niger", phone:08038246018}, {state: "PLATEAU", phone: 07032864444}]},
+            {name: 'NORTH-CENTRAL', subValues:[{state: "BENUE STATE", phone: 09018602439}, {state: "FCT", phone: 08099936312}, {state: "KOGI STATE", phone: 0704485619}, {state: "KWARA STATE", phone: 090620100001},{state: "Niger STATE", phone:08038246018}, {state: "PLATEAU STATE", phone: 07032864444}]},
             
-            {name: 'NORTHWEST', subValues:[{state: "KADUNA STATE", phone: 08977}, {state: "FCT", phone: 089787}, {state: "KOGI STATE", phone: 08977}, {state: "NIGER STATE", phone: 08977}, {state: "PLATEAU", phone: 94889}]},
+            {name: 'NORTH-WEST', subValues:[{state: "KADUNA STATE", phone: 08035871662}, {state: "KANO STATE", phone: 08039704476}, {state: "KATSINA STATE", phone: 09035037114}, {state: "SOKOTO STATE", phone: 08032311116}, {state: "ZAMFARA STATE", phone:08035626731 }]},
             
-            {name: 'NORTHEAST', subValues:[{state: "ADAMAWA STATE", phone: 08977}, {state: "FCT", phone: 089787}, {state: "KOGI STATE", phone: 08977}, {state: "NIGER STATE", phone: 08977}, {state: "PLATEAU", phone: 94889}]},
+            {name: 'NORTH-EAST', subValues:[{state: "ADAMAWA STATE", phone:08031230359}, {state: "BAUCHI STATE", phone: 08032717887}, {state: "BORNO STATE", phone: 08088159881}, {state: "GOMBE STATE", phone: 08103371257}, {state: "TARABA STATE", phone: 0803250165},{state:"YOBE STATE", phone: 08131834764} ]},
             
-            {name: 'SOUTHSOUTH', subValues:[{state: "AKWA-IBOM STATE", phone: 08977}, {state: "FCT", phone: 089787}, {state: "KOGI STATE", phone: 08977}, {state: "NIGER STATE", phone: 08977}, {state: "PLATEAU", phone: 94889}]}
+            {name: 'SOUTH-SOUTH', subValues:[{state: "AKWA-IBOM STATE", phone: 09045575515}, {state: "BAYELSA STATE", phone: 08039216821}, {state: "CROSS-RIVER STATE", phone: 09036281412}, {state: "DELTA STATE", phone: 08033521961}, {state: "EDO STATE", phone: 08084096723}, {state:  "RIVER STATE", phone:08056109538 } ]},
+
+            {name: 'SOUTH-EAST', subValues:[{state: "ABIA STATE", phone:07002242362 }, {state: "ANAMBRA STATE", phone:08030953771 }, {state: "ENUGU STATE", phone:08182555550 }, {state: "EBONYI STATE", phone:09020332489 }, {state: "IMO STATE", phone:08099555577 } ]},
+            
+            
+            {name: 'SOUTH-WEST', subValues:[{state: "OGUN STATE", phone: 08188978392 }, {state: "ONDO STATE", phone:070012684319 }, {state: "OSUN STATE", phone:08035025692 }, {state: "OYO STATE", phone:08038210122 }, {state: "EKITI STATE", phone:09062970434 }, {state:  "LAGOS STATE", phone:08023169485} ]},
+            
+            
         ]
 
         function makeSubmenu(value) {
