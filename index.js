794b28042bb210a9b105a655db4cf7dd182a201e
hafsat-max <hafsahbusari69@gmail.com>
2020-08-10 17:32:03 +0100

    alteration

---
+++ index.js
@@ -0,0 +1,68 @@
+/*let sentence = "This is meant to be a normal  sentence that  should be printed out backwards if it works"
+let reversed;
+
+function reverse() {
+    reversed = "";
+    sentence.split(' ').forEach(item => {
+        if (item.startsWith("a") || item.startsWith("e") || item.startsWith("i") || item.startsWith("o") || item.startsWith("u")) {
+            reversed += item + "pole "
+
+        } else {
+            reversed += item.substring(1) + item.substring(0, 1) + "yz "
+        }
+    });
+    return reversed;
+}
+document.write(reverse())*/
+
+/*let myArray = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]]
+document.write(myArray[0][0]);
+let array = [1, 2, 3, 4, 5];
+let newArray = [];
+
+function multiplyArray() {
+    array.forEach(item => {
+        if (item % 2 === 0) {
+            newArray.push(item * 2)
+        } else { newArray.push(item) }
+    })
+    return newArray;
+}
+console.log(multiplyArray());
+/*an array of 10 words, randomly pick one item from the array then*/
+
+/* const check = dash.split('').forEach((item) => {
+      const position = selectedWord.indexOf(input.value)
+      if (position > -1) {
+          dash = '';
+          selectedWord.split('').forEach(item => {
+              if (item === input.value) {
+                  dash += input.value + '  ';
+              } else {
+                  dash += '____  ';
+              }
+          })
+          display.innerHTML = dash;
+      
+      }
+  });
+
+  return check;*/
+
+/*
+for (var i = 1; i < 6; i++) {
+    console.log("@".repeat(i))
+}
+for (let i = 1; i <= 10; i++) {
+
+    console.log("#".repeat(i))
+
+}
+
+
+
+
+/*
+const aid = document.getElementById('aid')
+let ashArray = [1, 2, 3, 4, 5, 6, 7]
+let resultArray = []*/
